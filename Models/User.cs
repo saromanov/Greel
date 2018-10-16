db2c2ed371f6e42bcf4c27dce49d9000e0503f9a
Sergey <xxsmotur@gmail.com>
2018-10-17 02:30:23 +0500

    Add puppet for user model

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace Greel.Models {
+    public class User {
+        public Guid id {get;set;}
+        
+        public string FirstName {get;set}
+
+        public string LastName {get;set}
+    }
+}
